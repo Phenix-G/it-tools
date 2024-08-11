d6e855f6101631b3b36e3b374fd38e77ea658b72
Phenix-G <57041435+Phenix-G@users.noreply.github.com>
2024-08-12 00:01:42 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ import svgLoader from 'vite-svg-loader';
 import { configDefaults } from 'vitest/config';
 
 const baseUrl = process.env.BASE_URL ?? '/';
+// const baseUrl = process.env.BASE_URL ?? '/it-tools/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
